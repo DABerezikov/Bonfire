e467477f7e376b877dd96b30959bd4ccedb0df25
Дмитрий Березиков <dm.berezikov@yandex.ru>
2024-01-02 18:51:37 +0700

    Изменено отобрадение остатков семян, при 0 показывает пустое поле

--- Bonfire/Models/SeedsFromViewModel.cs
+++ Bonfire/Models/SeedsFromViewModel.cs
@@ -4,15 +4,28 @@ namespace Bonfire.Models;
 
 public class SeedsFromViewModel
 {
+   
+    private double? _AmountSeedsWeight;
+    private double? _AmountSeedsQuantity;
     internal int Id { get; set; }
     public string? Culture { get; set; }
     public string? Sort { get; set; }
     public string? Producer { get; set; }
     public DateTime ExpirationDate { get; set; }
     public double? WeightPack { get; set; }
-    public double? QuantityPack { get; set; }
-    public double? AmountSeedsWeight { get; set; }
-    public double? AmountSeedsQuantity { get; set; }
+    public double? QuantityPack { get; set;}
+
+    public double? AmountSeedsWeight
+    {
+        get => _AmountSeedsWeight != 0 ? _AmountSeedsWeight : null;
+        set => _AmountSeedsWeight = value;
+    }
+
+    public double? AmountSeedsQuantity
+    {
+        get => _AmountSeedsQuantity != 0 ? _AmountSeedsQuantity : null;
+        set => _AmountSeedsQuantity = value;
+    }
 
     public bool IsStillGood => (DateTime.Now.Year - ExpirationDate.Year) <= 0 && (DateTime.Now.Year - ExpirationDate.Year) > -1;
     public bool IsOld => (DateTime.Now.Year - ExpirationDate.Year) > 0;
