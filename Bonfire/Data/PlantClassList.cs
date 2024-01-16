492ed5b94b76b2f7c2e9823fea1a9a7dfd453443
Дмитрий Березиков <dm.berezikov@yandex.ru>
2024-01-16 23:32:08 +0700

    Рефакторинг Начать добавлять UnitTests

--- Bonfire/Data/PlantClassList.cs
+++ Bonfire/Data/PlantClassList.cs
@@ -4,8 +4,8 @@ namespace Bonfire.Data;
 
 public static class PlantClassList
 {
-    private static readonly List<string> _ClassList = new () { "Овощи", "Фрукты", "Ягоды", "Зелень", "Цветы" };
+    private static readonly List<string> ClassList = new() { "Овощи", "Фрукты", "Ягоды", "Зелень", "Цветы" };
 
-    public static List<string> GetClassList() => _ClassList;
+    public static IReadOnlyList<string> GetClassList() => ClassList;
 
 }
