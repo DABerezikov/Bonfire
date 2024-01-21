36fcfa3ddcb8f6cdac2dc380f30f740a4fb2e239
Дмитрий Березиков <dm.berezikov@yandex.ru>
2024-01-21 22:07:35 +0700

    Созданы классы для рассады и информации о ней, сформулированы требования к информации о рассаде

---
+++ BonfireDB/Entities/Seedling.cs
@@ -0,0 +1,11 @@
+﻿using BonfireDB.Entities.Base;
+
+namespace BonfireDB.Entities;
+
+public class Seedling: Entity
+{
+    public virtual Plant Plant { get; set; }
+    public virtual SeedlingInfo SeedlingInfo { get; set; }
+    public int SeedlingInfoId { get; set; }
+
+}
